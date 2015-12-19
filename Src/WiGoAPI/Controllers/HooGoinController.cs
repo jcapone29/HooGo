a8ff1d487c29c84b8a05c029ad6c53b288f3d00d
Joe Capone <josephcapone529@gmail.com>
2015-12-19 12:51:17 -0500

    added Controllers/HooGoinController.cs

---
+++ Src/WiGoAPI/Controllers/HooGoinController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Service;
+using Service.Repositories;
+
+namespace WiGoAPI.Controllers
+{
+    [RoutePrefix("api/HooGoin")]
+    public class HooGoinController : ApiController
+    {
+        private WiGoService _service;
+
+        public HooGoinController()
+        {
+            _service = new WiGoService();
+        }
+
+        [Route("userinfo")]
+        public async Task<IHttpActionResult> GetUserInfo(string username)
+        {
+            return Ok(await _service.GetUserInfo(username));
+        }
+
+    }
+}
